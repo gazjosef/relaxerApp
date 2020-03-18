516c26767cc84d863aed5d89b42248cf7f0a00db
gazjosef <39395712+gazjosef@users.noreply.github.com>
2020-03-18 15:07:01 +1100

    Breath Animation w/JS Trigger

---
+++ script.js
@@ -0,0 +1,20 @@
+const container = document.getElementById('container');
+const text = document.getElementById('text');
+
+const totalTime = 7500;
+const breathTime = (totalTime / 5) * 2;
+const holdTime = totalTime / 5;
+
+breathAnimation();
+
+function breathAnimation() {
+  console.log('Breath In!');
+
+  setTimeout(() => {
+    console.log('Hold');
+
+    setTimeout(() => {
+      console.log('Breath Out!');
+    }, holdTime);
+  }, breathTime);
+}
